a26472306d10133e62cfcf3131c4712928a4acae
Mark East <feasty@gmail.com>
2016-04-14 21:24:10 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from ui.main_ui import MainUI
+
+def main():
+    main_ui = MainUI()
+    main_ui.run()
+
+if __name__ == "__main__":
+    main()
---
+++ ui/main_ui.py
@@ -0,0 +1,7 @@
+
+class MainUI:
+    def __init__(self):
+        pass
+
+    def run(self):
+        print "Running main UI"
